62ffd5dfb1dd8b57d1150b5174aa0b836afe897b
Kim Ji Eun <{>
2024-11-15 15:23:57 +0900

    뉴스피드 프로젝트 셋업

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './pages/Login.jsx';
+import Home from './pages/Home.jsx';
+import MyPage from './pages/MyPage.jsx';
+import CreatePost from './pages/CreatePost.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/myPage" element={<MyPage />} />
+      <Route path="/home/create" element={<CreatePost />} />
+    </Routes>
+  </BrowserRouter>
+);
