3b483374a8bad7683c9a3ed1e85ec6bfeefdd197
hansolChoi29 <ggksthf29@gmail.com>
2024-11-15 21:56:08 +0900

    supabaceseting, reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
-function App() {
-  return <></>;
-}
+import { Reset } from 'styled-reset';
 
+function App() {
+  return (
+    <>
+      <Reset />
+    </>
+  );
+}
+//튜터님한테 app물어보기
 export default App;
