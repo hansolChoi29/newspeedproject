7c4fa1bdb8a03471fe027b699ce43be425f1dafc
hansolChoi29 <ggksthf29@gmail.com>
2024-11-18 18:46:17 +0900

    feat:mypage

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -5,7 +5,7 @@ import profileupdate from '../assets/profileupdate.png';
 import Pencil from '../assets/Pencil.png';
 import hart from '../assets/hart.png';
 import { supabase } from '../supabase/supabase';
-
+import history from '../assets/history.png';
 // 스타일 정의
 const Container = styled.form`
   width: 800px;
@@ -72,10 +72,10 @@ const Divider = styled.hr`
 `;
 
 const LikesSection = styled.div`
-  display: flex; /* Flexbox로 설정 */
-  align-items: center; /* 세로 정렬 */
-  justify-content: center; /* 가로 정렬 */
-  gap: 10px; /* 아이템 간 간격 */
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
   margin-bottom: 40px;
   font-size: 18px;
 
@@ -89,6 +89,7 @@ const LikesSection = styled.div`
 const PostList = styled.div`
   margin-top: 30px;
   width: 100%;
+  font-size: 20px;
 `;
 
 const Post = styled.div`
@@ -103,7 +104,9 @@ const PostAuthor = styled.div`
   font-weight: bold;
   margin-bottom: 5px;
 `;
-
+const History = styled.img`
+  width: 30px;
+`;
 // MyPage 컴포넌트
 const MyPage = () => {
   const [profileImage, setProfileImage] = useState(myprofile);
@@ -114,6 +117,7 @@ const MyPage = () => {
 
   useEffect(() => {
     const fetchData = async () => {
+      // 유효성검사해서 로그인한 건지 확인해줘야 함. 코드생성.
       try {
         // 1. `users` 테이블에서 닉네임 가져오기
         const { data: userData, error: userError } = await supabase.from('users').select('user_nick_name').single();
@@ -129,9 +133,7 @@ const MyPage = () => {
 
         // 3. `likes` 테이블에서 좋아요 수 가져오기
         const { data: likesData, error: likesError } = await supabase.from('likes').select('likes_count');
-
         if (likesError) throw likesError;
-
         const totalLikes = likesData.reduce((sum, like) => sum + like.likes_count, 0);
         setLikesCount(totalLikes); // 좋아요 수 설정
       } catch (error) {
@@ -168,16 +170,18 @@ const MyPage = () => {
         </NicknameContainer>
         <Divider />
       </Section>
-
       <LikesSection>
-        <img src={hart} alt="Likes" width="30px" />
+        <History src={hart} alt="Likes" width="30px" />
         <h2>
-          <span>+{likesCount}</span>
+          <span>{likesCount}</span>
         </h2>
       </LikesSection>
 
       <PostList>
-        {posts.map((post) => (
+        <p>
+          <History src={history} alt="history" /> 내가 쓴 게시글
+        </p>
+        {posts.slice(0, 3).map((post) => (
           <Post key={post.id}>
             <PostAuthor> {nickname}</PostAuthor>
             <div>{post.post_contents}</div>
