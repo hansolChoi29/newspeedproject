7f374e48afe62723eccf24091603f67eb5c186e0
Kim Ji Eun <{>
2024-11-19 12:16:14 +0900

    app.jsx 헤더 컨포넌트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,13 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
